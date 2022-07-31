13aeb1d0c11dac0a08491c78019a95c7ce4607ef
Jon Bird <attid@Jons-Laptop>
2022-07-31 16:24:22 -0600

    add apiRoutes.js

---
+++ routes/apiRoutes.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const router = require('express').Router();
+const fs = require('fs');
+const db = require('../db/db.json');
+const { v4: uuidv4 } = require('uuid');
+
+router.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '../db/db.json'));
+});
+
+
+router.post('/notes', (req, res) => {
+  let save = req.body;
+  save.id = uuidv4();
+  db.push(save);
+  fs.writeFile(path.join(__dirname, '../db/db.json'),
+  JSON.stringify(db),
+  function(err) {
+    if(err) {
+      console.log(err);
+      res.status(500).send('please try again.')
+    } else {
+      res.json(save)
+    };
+  }
+  );
+});
+
+module.exports = router;
